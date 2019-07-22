304305fd82541bf8c3aeb5c75efb3ac01ac25c5c
Jeet Dholakia <jeet.dholakia@peaas.co>
2019-07-22 15:12:51 +0530

    Removed derived data from framework

--- DerivedData/CommonUtils/Build/Intermediates.noindex/CommonUtils.build/Debug-iphonesimulator/CommonUtils.build/DerivedSources/CommonUtils_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char CommonUtilsVersionString[];
- extern const double CommonUtilsVersionNumber;
-
- const unsigned char CommonUtilsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CommonUtils  PROJECT:CommonUtils-1" "\n";
- const double CommonUtilsVersionNumber __attribute__ ((used)) = (double)1.;
